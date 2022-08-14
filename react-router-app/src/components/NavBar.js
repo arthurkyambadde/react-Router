d250334281077a1c1d8ba87f2d87fa5280a008f0
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-15 02:13:19 +0300

    feat: add navbar

---
+++ react-router-app/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import "../index.css";
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="about">About</NavLink>
+    </nav>
+  );
+};
