71d00297d590ede8aeb0731e1f77b6b7ce579344
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-15 02:55:55 +0300

    feat: add a NoMatch route condition

--- react-router-app/src/App.js
+++ react-router-app/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { About } from "./components/about";
 import { Home } from "./components/home";
 import { NavBar } from "./components/NavBar";
+import { NoMatch } from "./components/NoMatch";
 import { OrderSummary } from "./components/OrderSummarry";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="order-summary" element={<OrderSummary />}></Route>
         <Route path="about" element={<About />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </div>
   );
