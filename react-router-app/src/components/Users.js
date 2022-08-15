de586a0fff67815941a459f14e89d0f0e9eb3036
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-15 21:31:35 +0300

    feat: add search params concept

--- react-router-app/src/components/Users.js
+++ react-router-app/src/components/Users.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter") === "active";
+
   return (
     <div>
       <div>user 1</div>
       <div>user 2</div>
       <div>user 3</div>
+      <Outlet />
+      <div>
+        <button
+          onClick={() => {
+            setSearchParams({ filter: "active" });
+          }}
+        >
+          Active Users
+        </button>
+        <button
+          onClick={() => {
+            setSearchParams({});
+          }}
+        >
+          Active Users
+        </button>
+      </div>
+      {showActiveUsers ? (
+        <h2>showing active users</h2>
+      ) : (
+        <h2>showing all users</h2>
+      )}
     </div>
   );
 };
